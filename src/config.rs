86196728f17f481416213a4ab979a1b9ab65300c
Sasha Matijasic <sasha@selectnull.com>
2023-05-22 10:24:04 +0200

    Refactor config to new module

---
+++ src/config.rs
@@ -0,0 +1,38 @@
+use serde::Deserialize;
+use std::env;
+use std::path::{PathBuf, Path};
+use std::fs::File;
+use std::io::Read;
+use toml;
+
+#[derive(Deserialize)]
+pub struct Config {
+    pub base_url: Option<String>,
+}
+
+fn get_config_file_path() -> Option<PathBuf> {
+    let mut path = env::current_dir().expect("Failed to get current directory");
+    let root = Path::new("/");
+
+    loop {
+        let config_path = path.join(".endpoint.toml");
+        if config_path.exists() && config_path.is_file() {
+            return Some(config_path);
+        }
+        if path == root {
+            return None;
+        }
+        path.pop();
+    }
+}
+
+pub fn read_config() -> Config {
+    let config_path = match get_config_file_path() {
+        Some(path) => path,
+        None => return Config { base_url: None },
+    };
+
+    let mut config_string = String::new();
+    File::open(&config_path).unwrap().read_to_string(&mut config_string).unwrap();
+    toml::from_str(&config_string).unwrap()
+}
--- src/main.rs
+++ src/main.rs
@@ -2,15 +2,9 @@ use reqwest::{Client, Response, Method, Url, Body, header::CONTENT_TYPE};
 use std::env;
 use std::fs::File;
 use std::io::Read;
-use serde::Deserialize;
-use std::path::{PathBuf, Path};
 use tokio;
-use toml;
 
-#[derive(Deserialize)]
-struct Config {
-    base_url: Option<String>,
-}
+mod config;
 
 async fn send_request(method: Method, url: Url, body: Option<String>) -> Result<Response, reqwest::Error> {
     let client = Client::new();
@@ -23,34 +17,6 @@ async fn send_request(method: Method, url: Url, body: Option<String>) -> Result<
     req_builder.send().await
 }
 
-fn get_config_file_path() -> Option<PathBuf> {
-    let mut path = env::current_dir().expect("Failed to get current directory");
-    let root = Path::new("/");
-
-    loop {
-        let config_path = path.join(".endpoint.toml");
-        // println!("{}", &path.display());
-        if config_path.exists() && config_path.is_file() {
-            return Some(config_path);
-        }
-        if path == root {
-            return None;
-        }
-        path.pop();
-    }
-}
-
-fn read_config() -> Config {
-    let config_path = match get_config_file_path() {
-        Some(path) => path,
-        None => return Config { base_url: None },
-    };
-    // println!("Config path: {}", &config_path.display());
-
-    let mut config_string = String::new();
-    File::open(&config_path).unwrap().read_to_string(&mut config_string).unwrap();
-    toml::from_str(&config_string).unwrap()
-}
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -66,8 +32,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let url = if args[2].starts_with("http") {
         args[2].clone()
     } else {
-        let config = read_config();
-        // println!("{}", &config.base_url.as_ref().unwrap());
+        let config = config::read_config();
         match config.base_url {
             Some(base_url) => format!("{}{}", base_url, args[2]),
             None => args[2].clone(),
